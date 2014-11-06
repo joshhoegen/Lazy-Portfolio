f6c75d9fa8ed2130822947123662ce7269a71765
joshhoegen <joshhoegen@gmail.com>
2014-11-06 11:03:39 -0500

    adding instagram functionality

--- app/js/modules/aggr/aggr.js
+++ app/js/modules/aggr/aggr.js
@@ -15,18 +15,33 @@ aggr.controller('aggrList', ['$scope', '$sce', 'aggr',
     SC.initialize({
         client_id: 'b2d19575a677c201c6d23c39e408927a'
     });
-    SC.get('/resolve', {
+    var sc = SC.get('/resolve', {
         url: 'http://soundcloud.com/byutifu/tracks',
         limit: 10
     }, function (track) {
-        track = track.slice(0, 3);
+        var output = {};
         angular.forEach(track, function(v, k) {
             console.log(v);
-            track[k].widget_url = 'https://w.soundcloud.com/player/?url=' +
-            encodeURIComponent(v.uri) +
-            '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true';    
+            track = track.slice(0, 3);
+            output[k].title = v.title;
+            output[k].date = v.last_modified;
+            output[k].embed_url = 'https://w.soundcloud.com/player/?url=' +
+                encodeURIComponent(v.uri) +
+                '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true';    
         });
-        $scope.aggr = track;
-    });    
+        return track;
+    }),
+    insta = function(){
+        var cacheImage, html = '';
+        return $.getJSON('https://api.instagram.com/v1/users/' + account.user.id + '/media/recent?access_token=' + account.access_token + '&count=' + count + '&callback=?', function(data) {
+            angular.forEach(data.data, function(i, image) {
+                html += '<img src="'+image.images.low_resolution.url+'" alt="'+image.images.title+'" />';
+            });
+            html += html+html+html+html+html;
+            html += html //co-co-combo breaker!
+            $('.instagram').append(html);
+        });
+    }
+    $scope.aggr = items;
   }]);
 
