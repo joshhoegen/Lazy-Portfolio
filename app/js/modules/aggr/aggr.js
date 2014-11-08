f4925a8d0cae1421a0a3570452153a9c73ad3c04
joshhoegen <joshhoegen@gmail.com>
2014-11-08 18:36:20 -0500

    finished modularizing external api services

--- app/js/modules/aggr/aggr.js
+++ app/js/modules/aggr/aggr.js
@@ -1,36 +1,62 @@
-'use strict';
+var aggr = angular.module('aggr', ['ngResource', 'soundcloud', 'instagram']);
 
-var aggr = angular.module('aggr', ['ngResource', 'soundcloud']);
-
-aggr.factory('aggr', function(){
-  return {
-    getSc: function(){
-      console.log('+');
+aggr.factory('aggr', function() {
+    return {
+        getSc: function() {
+            console.log('+');
+        }
     }
-  }  
 });
 
-aggr.filter('unsafe', function($sce) { return $sce.trustAsResourceUrl; });
+aggr.filter('unsafe', function($sce) {
+    return $sce.trustAsResourceUrl;
+});
 
-aggr.controller('aggrList', ['$scope', '$sce', '$http', 'aggr', 'getSoundcloud',
-  function($scope, $sce, $http, aggr, getSoundcloud) {
-    $scope.aggr = [];
-    //console.log(getSoundcloud());
-    //var sc = getSoundcloud();
+aggr.controller('aggrList', ['$scope', '$sce', '$http', 'aggr', 'getSoundcloud', 'getInstagram',
+    function($scope, $sce, $http, aggr, getSoundcloud, getInstagram) {
+        $scope.aggr = [];
+        var instagram = getInstagram.getImages(),
+            scTracks = getSoundcloud.getTracks('http://soundcloud.com/byutifu/tracks');
+        instagram.then(
+            function(images) {
+                var output = [];
+                angular.forEach(images.data, function(v, k) {
+                    var dateFormat = new Date(parseInt(v.created_time) * 1000).getTime();
+                    output[k] = {
+                        'title': v.caption.text,
+                        'date': dateFormat,
+                        'embed_url': v.images.standard_resolution.url,
+                        'type': 'image'
+                    };
+                    
+                });
+                $scope.aggr.push.apply($scope.aggr, output);
+            },
+            function(error) {
+                console.log('failure loading Instagram' + error);
+            }
+        );
 
-    
-    var scTracks = getSoundcloud.getTracks('https://api.soundcloud.com/users/1781996/tracks.json?client_id=b2d19575a677c201c6d23c39e408927a', insta);
-    scTracks.then(function(payload) {
-        console.log(payload);
-        $scope.aggr.push.apply($scope.aggr, payload);
-        console.log($scope.aggr);
-    },
-    function(errorPayload) {
-        console.log('failure loading movie');
-    });
-    ;
-    //console.log(scTracks);
-    //console.log($scope);
-    //console.log(insta());
-  }]);
-
+        scTracks.then(
+            function(tracks) {
+                var output = [];
+                tracks = tracks.slice(0, 8);
+                angular.forEach(tracks, function(v, k) {
+                    var dateFormat = new Date(v.created_at).getTime();
+                    output[k] = {
+                        'title': v.title,
+                        'type': 'music',
+                        'date': dateFormat,
+                        'embed_url': 'https://w.soundcloud.com/player/?url=' +
+                            encodeURIComponent(v.uri) +
+                            '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
+                    };
+                });
+                $scope.aggr.push.apply($scope.aggr, output);
+            },
+            function(error) {
+                console.log('failure loading SoundCloud' + error);
+            }
+        );
+    }
+]);
--- app/js/modules/soundcloud/soundcloud.js
+++ app/js/modules/soundcloud/soundcloud.js
@@ -1,61 +1,21 @@
-angular.
- module('soundcloud', []).
- factory('getSoundcloud', function($http, $log, $q) {
-   return {
-      
-            getUrl: function(url, callback){
-               var gsc = this;
-               $http.get('http://api.soundcloud.com/resolve', {
-                  params: {
-                     url: url || 'http://soundcloud.com/byutifu/tracks',
-                     client_id: 'b2d19575a677c201c6d23c39e408927a',
-                     format: 'json',
-                     '_status_code_map[302]':'200'
-                  }
-               }).success(function(result) {
-                  console.log(result);
-                  return gsc.getTracks(result.data.location, callback);
-               });
-            },
-            getTracks: function(url, callback) {
-               var deferred = $q.defer();
-               $http.get(url || getUrl(url), {
-                      params: {
-                         limit: 8,
-                         client_id: 'b2d19575a677c201c6d23c39e408927a',
-                         format: 'json',
-                         '_status_code_map[302]':'200'
-                      }
-                  }).success(function(result) {
-                  //resolve the promise as the data
-                  var output = [],
-                     track = result;
-                  console.log(track);
-                  track = track.slice(0, 8);
-                  angular.forEach(track, function(v, k) {
-                      console.log(v);
-                      var dateFormat = new Date(v.created_at).getTime();
-                      output[k] = {
-                          'title': v.title,
-                          'type': 'music',
-                          'date': dateFormat,
-                          'embed_url': 'https://w.soundcloud.com/player/?url=' +
-                          encodeURIComponent(v.uri) +
-                          '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
-                      };
-                  });
-                  if (callback) {
-                     callback();
-                  }
-                  deferred.resolve(output);
-               });
-               console.log(deferred.promise);
-               return deferred.promise;
-            }
-         
-   }
-}).controller('controlSoundCloud', function($scope, getSoundcloud) {
-    getSoundcloud.getTracks().then(function(tracks) {
-        $scope.soundcloud = tracks;
-    });
-});;
+angular.module('soundcloud', []).
+   factory('getSoundcloud', function($http, $log, $q) {
+      return {
+         getTracks: function(url, callback) {
+            var gsc = this,
+               deferred = $q.defer();
+            SC.initialize({
+               client_id: 'b2d19575a677c201c6d23c39e408927a'
+            }).get('/resolve', {
+               url: url,
+               limit: 8
+            },function(tracks) {
+               if (callback) {
+                  callback();
+               }
+               deferred.resolve(tracks);
+            });
+            return deferred.promise;
+         }
+      }
+   });
