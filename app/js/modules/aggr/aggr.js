8d89edb8a6877ce30acd5792256e25b99948cc69
joshuahoegen <joshuahoegen@l000179.local>
2017-06-23 15:44:49 -0400

    Removing social media likes.

--- app/js/modules/aggr/aggr.js
+++ app/js/modules/aggr/aggr.js
@@ -67,18 +67,18 @@ aggr.factory('aggrConfig', ['$resource', '$http', '$q', 'getSoundcloud', 'getIns
             console.log('failure loading Instagram' + error);
         },
         social = function() {
-           (function() {
-             var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-             po.src = 'https://apis.google.com/js/platform.js';
-             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-           })();
-           (function(d, s, id) {
-             var js, fjs = d.getElementsByTagName(s)[0];
-             if (d.getElementById(id)) return;
-             js = d.createElement(s); js.id = id;
-             js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0';
-             fjs.parentNode.insertBefore(js, fjs);
-           }(document, 'script', 'facebook-jssdk'));
+          //  (function() {
+          //    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+          //    po.src = 'https://apis.google.com/js/platform.js';
+          //    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          //  })();
+          //  (function(d, s, id) {
+          //    var js, fjs = d.getElementsByTagName(s)[0];
+          //    if (d.getElementById(id)) return;
+          //    js = d.createElement(s); js.id = id;
+          //    js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0';
+          //    fjs.parentNode.insertBefore(js, fjs);
+          //  }(document, 'script', 'facebook-jssdk'));
         };
 
         aggrConfig.cache({json: 'true'}).then(function(data){
@@ -175,7 +175,7 @@ aggr.factory('aggrConfig', ['$resource', '$http', '$q', 'getSoundcloud', 'getIns
                     }
                 );
             }
-            social();
+            // social();
         });
     }
 ]);
