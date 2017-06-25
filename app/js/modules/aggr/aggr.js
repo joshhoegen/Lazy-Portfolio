19ccbd707f8d5af4b367ffa512f55881136c45fc
joshuahoegen <joshuahoegen@l000179.local>
2017-06-25 19:26:45 -0400

    Quick g+ changes.

--- app/js/modules/aggr/aggr.js
+++ app/js/modules/aggr/aggr.js
@@ -21,14 +21,10 @@ aggr.factory('aggrConfig', ['$resource', '$http', '$q', 'getSoundcloud', 'getIns
         // will accept object to filter/isolate feeds feed('soundcloud') or feed(['soundcloud', 'instagram'])
         aggrConfig.feed = function(){
             var deferred = $q.defer(),
-                mobilecheck = function() {
-                    var check = false;
-                    (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
-                    return check;
-                },
+                mobilecheck = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
                 feeds = {
                     instagram: getInstagram.getImages(aggrConfig.load('instagram'), (mobilecheck ? 8 : 12)),
-                    flickr: getFlickr.getImages(aggrConfig.load('flickr'), (mobilecheck ? 3 : 6)),
+                    flickr: getFlickr.getImages(aggrConfig.load('flickr'), (mobilecheck ? 4 : 6)),
                     soundcloud: getSoundcloud.getTracks(aggrConfig.load('soundcloud'), (mobilecheck ? 4 : 8)),
                     google: getGoogle.getGoogle(aggrConfig.load('google'), 5)
                 };
@@ -115,7 +111,8 @@ aggr.factory('aggrConfig', ['$resource', '$http', '$q', 'getSoundcloud', 'getIns
                         angular.forEach(images.photos.photo, function(v, k) {
                             var dateFormat = new Date(parseInt(v.lastupdate)).getTime();
                             output[k] = formatOutput('flickr',
-                                v.title, dateFormat, 'image',
+                                // Flickr Needs * 1000
+                                v.title, (dateFormat * 1000), 'image',
                                 'https://farm' + v.farm +
                                     '.staticflickr.com/' + v.server +
                                     '/' + v.id + '_' + v.secret + '_c.jpg',
@@ -159,7 +156,7 @@ aggr.factory('aggrConfig', ['$resource', '$http', '$q', 'getSoundcloud', 'getIns
                             var attachments = v.attachments && v.attachments.length ? v.attachments : false;
 
                             if (attachments && attachments[0].fullImage) {
-                              v.content += '<br/><img src="' + attachments[0].fullImage.url+ '" />';
+                              v.content += '<img class="google-img" src="' + attachments[0].fullImage.url+ '" />';
                             }
 
                             output[k] = formatOutput('google',
