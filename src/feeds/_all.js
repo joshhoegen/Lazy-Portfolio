5e162181a87eb977dd8bc93191e3696d0efa12dd
joshuahoegen <josh.hoegen@natgeo.com>
2019-01-19 21:13:38 -0500

    Added prettier and eslint

--- src/feeds/_all.js
+++ src/feeds/_all.js
@@ -3,15 +3,15 @@ import flickrFeed from './flickr'
 import soundcloudFeed from './soundcloud'
 // import twitter from './twitter'
 
+/* eslint-disable class-methods-use-this */
 export default class Aggr {
-  constructor(opts) {
-
-  }
   aggrAll() {
-    return Promise.all([googleFeed, flickrFeed, soundcloudFeed]).then((result) => {
-      let agr = [].concat(...result)
+    return Promise.all([googleFeed, flickrFeed, soundcloudFeed]).then(result => {
+      const agr = [].concat(...result)
+
       // sort newest to oldest
-      return agr.sort((a, b) => b.date.toString().localeCompare(a.date));
+      return agr.sort((a, b) => b.date.toString().localeCompare(a.date))
     })
   }
 }
+/* eslint-enable class-methods-use-this */
